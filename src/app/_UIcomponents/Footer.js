7fcbefd88e917a72f303fed541603948a3d2d4d7
Akash Paloju <akashpaloju@gmail.com>
2024-06-28 19:41:03 +0530

    add navbar, footer, initial events page

---
+++ src/app/_UIcomponents/Footer.js
@@ -0,0 +1,42 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import {
+	faGithub,
+	faInstagram,
+	faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
+
+const Footer = () => {
+	return (
+		<div className="w-screen flex justify-center py-4">
+			<div className="flex justify-between w-[80%] sm:w-1/2">
+				<a
+					href="https://www.instagram.com/bitbyte.tpc/"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<FontAwesomeIcon icon={faInstagram} className="h-6 w-6 text-white" />
+				</a>
+				<a href="mailto:theprogclub@iiitdmj.ac.in">
+					<FontAwesomeIcon icon={faEnvelope} className="h-6 w-6 text-white" />
+				</a>
+				<a
+					href="https://in.linkedin.com/company/bitbyte-tpc"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<FontAwesomeIcon icon={faLinkedin} className="h-6 w-6 text-white" />
+				</a>
+				<a
+					href="https://github.com/BitByte-TPC"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<FontAwesomeIcon icon={faGithub} className="h-6 w-6 text-white" />
+				</a>
+			</div>
+		</div>
+	);
+};
+
+export default Footer;
